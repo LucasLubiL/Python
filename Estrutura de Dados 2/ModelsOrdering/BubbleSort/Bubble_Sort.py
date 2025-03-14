b92eae92d6ff7305bcf81522a3431032b71f0ed0
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2025-03-14 13:52:40 -0300

    Add files via upload

--- Estrutura de Dados 2/ModelsOrdering/BubbleSort/Bubble_Sort.py
+++ Estrutura de Dados 2/ModelsOrdering/BubbleSort/Bubble_Sort.py
@@ -3,13 +3,16 @@ from func import order
 print("Digite 10 numeros para poder ordenar: ")
 
 vetor = []
+tam = [0]
 
 for i in range(10):
     x = int(input())
     vetor.append(x)
 
-order(vetor)
+order(vetor, tam)
 
 print("Numeros ordenados")
 for i in range(10):
     print(vetor[i])
+
+print("Quantiadade passadas: ", tam[0])
--- Estrutura de Dados 2/ModelsOrdering/BubbleSort/func.py
+++ Estrutura de Dados 2/ModelsOrdering/BubbleSort/func.py
@@ -1,6 +1,7 @@
-def order (vetor):
+def order (vetor, tam):
     for i in range(10-1):
         for j in range(10-1-i):
+            tam[0] += 1
             if vetor[j] > vetor[j+1]:
                 aux = vetor[j]
                 vetor[j] = vetor[j+1]
