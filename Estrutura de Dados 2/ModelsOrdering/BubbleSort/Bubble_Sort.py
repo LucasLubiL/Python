6a6d7883b9df8e71c30745568acc98bc90aca0fb
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2025-03-15 13:55:18 -0300

    Add files via upload

--- Estrutura de Dados 2/ModelsOrdering/BubbleSort/Bubble_Sort.py
+++ Estrutura de Dados 2/ModelsOrdering/BubbleSort/Bubble_Sort.py
@@ -5,14 +5,21 @@ print("Digite 10 numeros para poder ordenar: ")
 vetor = []
 tam = [0]
 
+# Neste for se faz a leitura do inteiro para se colocar no vetor;
 for i in range(10):
     x = int(input())
     vetor.append(x)
 
+# vetor nao ordenado;
+print("Numeros NÃO ordenados:", end=" ")
+print(" ".join(map(str, vetor)))
+
+# Aqui chama-se uma função com os 2 parametros, vetor e um contador;
 order(vetor, tam)
 
-print("Numeros ordenados")
-for i in range(10):
-    print(vetor[i])
+# Já o vetor em ordem crescente;
+print("Numeros ordenados:", end=" ")
+print(" ".join(map(str, vetor)))
 
+# Quantas vezes houve a iteração.
 print("Quantiadade passadas: ", tam[0])
--- Estrutura de Dados 2/ModelsOrdering/BubbleSort/func.py
+++ Estrutura de Dados 2/ModelsOrdering/BubbleSort/func.py
@@ -1,6 +1,9 @@
+# Aqui faz toda a verificação do vetor, analisando se um numero é maior que o numero da proxima posição;
+
 def order (vetor, tam):
     for i in range(10-1):
         for j in range(10-1-i):
+            # Este 'tam' é acrescentado cada vez que se tem uma interção com o for, para se saber quantas vezes ele foi executado (FORMA OTIMIZADA).
             tam[0] += 1
             if vetor[j] > vetor[j+1]:
                 aux = vetor[j]
