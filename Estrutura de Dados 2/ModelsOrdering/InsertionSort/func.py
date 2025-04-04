534baae7e431b7ad34f21dd4bd2b3f3f0210da0a
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2025-04-04 12:50:56 -0300

    Update func.py

--- Estrutura de Dados 2/ModelsOrdering/InsertionSort/func.py
+++ Estrutura de Dados 2/ModelsOrdering/InsertionSort/func.py
@@ -1,10 +1,11 @@
 def order (vetor, tam):
     aux = 0
-    for i in range(10):
+    n = len(vetor)
+    for i in range(n):
         aux = vetor[i]
         j = i - 1
         while j >= 0 and vetor[j]>aux:
             vetor[j+1] = vetor[j]
             j = j - 1
+            tam[0] = tam[0] + 1
         vetor[j + 1] = aux
-            
